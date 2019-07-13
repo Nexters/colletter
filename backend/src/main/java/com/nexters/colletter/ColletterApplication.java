f030198ff8b2c3ea02188388582c2295884a404f
KIMSJ <leesd556@gmail.com>
2019-07-13 18:01:46 +0900

    set default backend

---
+++ backend/src/main/java/com/nexters/colletter/ColletterApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.colletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColletterApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/nexters/colletter/ColletterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nexters.colletter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ColletterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
