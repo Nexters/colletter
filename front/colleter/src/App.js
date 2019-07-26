c115ab8f324e2ce9d6de58698afb831fd62b65c2
wkdtndgns <wkdtndgns@naver.com>
2019-07-26 21:46:41 +0900

    기본 로그인 구조

--- front/colleter/src/App.js
+++ front/colleter/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import home from './views/home/home';
+import login from './views/login/login';
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-    </div>
-  );
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <div>
+                        <Route exact path="/" component={login}/>
+                        <Route exact path="/home" component={home}/>
+                    </div>
+                </Router>
+            </div>
+        )
+    };
 }
 
 export default App;
