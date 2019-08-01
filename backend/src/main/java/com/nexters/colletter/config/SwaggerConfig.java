2c866d7e94003380dba5b630dff4f3e9cb550c8e
KIMSJ <leesd556@gmail.com>
2019-08-01 19:48:40 +0900

    add swagger

---
+++ backend/src/main/java/com/nexters/colletter/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.nexters.colletter.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+
+/**
+ * Swagger 자동 문서화 툴을 위한 설정파일.
+ * TODO : authentication
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                // error.* 제외. 스프링에서 기본 설정된것.
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+}
