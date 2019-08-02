e1189a24570cd9e8767f08649a5893190fba50e0
KIMSJ <leesd556@gmail.com>
2019-08-02 21:43:09 +0900

    set travis docker

--- travis/docker-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
-docker build -t colletter:latest --build-arg JAR_FILE=build/libs/*.jar .
-docker tag colletter:latest ${DOCKER_USERNAME}/colletter:latest
-docker push ${DOCKER_USERNAME}/colletter:latest
