e3370c53779cc9b7cfd4ce88d2930a48dd9a448e
hzoou <woojooo52@naver.com>
2019-08-04 16:36:41 +0900

    mypage card gui 수정

--- front/colleter/src/views/mypage/mypage_bookmarkList.js
+++ front/colleter/src/views/mypage/mypage_bookmarkList.js
@@ -1,83 +1,164 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Card, CardDeck } from 'react-bootstrap';
+import { Card, CardColumns } from 'react-bootstrap';
 import heart from '../../img/ic-heart-picked.png';
 import card from '../../img/cardImg.PNG';
 
 import '../../css/mypage_bookmarkList.css'
 
 const CardCategory = styled.span`
-  font-family: NotoSansCJKkr;
-  font-size: 14px;
-  font-weight: 300;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #3bd277;
+    font-family: NotoSansCJKkr;
+    font-size: 18px;
+    font-weight: 300;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #3bd277;
 `;
 
 const CardCount = styled.span`
-   font-family: NotoSansCJKkr;
-  font-size: 14px;
-  font-weight: 300;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #686868;
-  margin-left : 15px;
+    font-family: NotoSansCJKkr;
+    font-size: 18px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #4e4e4e;
+    margin-left : 15px;
 `;
+
+const CardFavText = styled.span`
+    font-weight: 300;
+    color: #686868;
+`
 
 class BookmarkList extends Component {
     render() {
         return (
-        <CardDeck>
-            <Card style={{width: '18rem'}}>
-                <Card.Body>
-                    <Card.Img variant="right" className="heartImg" src={heart}/>
-                    <Card.Img variant="right" className="cardImg" src={card}/>
+            <CardColumns>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
 
-                    <Card.Title className="cardTitle">뉴닉</Card.Title>
-                    <Card.Text className="cardText">
-                        금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
                     </Card.Text>
 
-                    <Card.Text className="cardText">
-                        <CardCategory>정치/경제</CardCategory><CardCount>좋아요 4</CardCount>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            <Card style={{width: '18rem'}}>
-                <Card.Body>
-                    <Card.Img variant="right" className="heartImg" src={heart}/>
-                    <Card.Img variant="right" className="cardImg" src={card}/>
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
 
-                    <Card.Title className="cardTitle">뉴닉</Card.Title>
-                    <Card.Text className="cardText">
-                        금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
                     </Card.Text>
 
-                    <Card.Text className="cardText">
-                        <CardCategory>정치/경제</CardCategory><CardCount>좋아요 4</CardCount>
-                    </Card.Text>
-                </Card.Body>
-            </Card> <Card style={{width: '18rem'}}>
-            <Card.Body>
-                <Card.Img variant="right" className="heartImg" src={heart}/>
-                <Card.Img variant="right" className="cardImg" src={card}/>
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
 
-                <Card.Title className="cardTitle">뉴닉</Card.Title>
-                <Card.Text className="cardText">
-                    금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
                 </Card.Text>
 
-                <Card.Text className="cardText">
-                    <CardCategory>정치/경제</CardCategory><CardCount>좋아요 4</CardCount>
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
+
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
+                    </Card.Text>
+
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
+
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
+                    </Card.Text>
+
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
+
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
                 </Card.Text>
-            </Card.Body>
-        </Card>
-        </CardDeck>
+
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
+
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
+                    </Card.Text>
+
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Img variant="right" className="heartImg" src={heart} />
+                        <Card.Img variant="right" className="cardImg" src={card} />
+
+                        <Card.Title className="cardTitle">뉴닉</Card.Title>
+                        <Card.Text className="cardDesc">
+                            금주의 핫한 디자인을 빠르게 받아볼 수 있는 디자인 뉴스레터 입니다.
+                    </Card.Text>
+
+                        <Card.Text className="cardText">
+                            <CardCategory>정치/경제</CardCategory><CardCount><CardFavText>좋아요 </CardFavText>4</CardCount>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardColumns>
         )
     }
 }
